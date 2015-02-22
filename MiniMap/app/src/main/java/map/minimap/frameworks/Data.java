42ec125e9717369dccc5e0f2ee3c5408b7ad29ec
mtracy01 <matthetracy@gmail.com>
2015-02-21 23:18:52 -0500

    Created Data class for storing data accessed across the applicaton

---
+++ MiniMap/app/src/main/java/map/minimap/frameworks/Data.java
@@ -0,0 +1,30 @@
+package map.minimap.frameworks;
+import android.app.Application;
+/**
+ * Created by Matthew on 2/21/2015.
+ * Purpose: Store global data that is used throughout the application
+ */
+public class Data extends Application {
+
+    //Things for the application to store
+    private User user;
+    private User[] users;
+
+    public void setUser(User user){
+        this.user = user;
+    }
+    public User getUser(){
+        return user;
+    }
+
+    public void setUserList(User[] userList){
+        int length = userList.length;
+        this.users = new User[length];
+        System.arraycopy(userList,0,this.users,0,length);
+    }
+
+    public User[] getUserList(){
+        return users;
+    }
+
+}
