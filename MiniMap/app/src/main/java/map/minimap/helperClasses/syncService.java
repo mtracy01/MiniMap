052e307384ee8fc5852dbb853af05b541f467f65
mtracy01 <matthetracy@gmail.com>
2015-02-24 20:45:49 -0500

    Created Asynchronous class for executing background services

---
+++ MiniMap/app/src/main/java/map/minimap/helperClasses/syncService.java
@@ -0,0 +1,25 @@
+package map.minimap.helperClasses;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Matthew on 2/24/2015.
+ * Purpose: Run ****SMALL**** Background tasks at specified intervals for the system to request information.
+ * Use primarily for notification services
+ */
+public class syncService extends AsyncTask{
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+
+    /**
+     * Purpose: Check if we have new invites every few seconds
+     */
+    {
+
+    }
+    public static void inviteCheck(){
+
+    }
+}
