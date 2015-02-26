9aacd101445cdcd98565dda778803f6cc8fee0bd
Joe <joe.coy@comcast.net>
2015-02-26 09:35:33 -0500

    wrote simple beacon and location classes

--- Server/src/server/Beacon.java
+++ Server/src/server/Beacon.java
@@ -5,6 +5,28 @@ import java.util.ArrayList;
 public class Beacon {
 
 	private static Integer baseId = 0;
+	private Location location;
+	private int id;
 	
+	public Beacon (Location loc)
+	{
+		setLocation(loc);
+		
+		synchronized (baseId) {
+			id = baseId;
+			baseId++;
+		}
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
 	
+	public int getId() {
+		return id;
+	}
 }
--- Server/src/server/Location.java
+++ Server/src/server/Location.java
@@ -1,5 +1,30 @@
 package server;
 
 public class Location {
+	private double longitude;
+	private double latitude;
+	
+	public Location (double lon, double lat)
+	{
+		setLongitude(lon);
+		setLatitude(lat);
+	}
 
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	
 }
