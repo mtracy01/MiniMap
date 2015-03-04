0db5abb465bb33b1f3f5c6a39ea788a3f0536786
Michael Crabill <michael.crabill@gmail.com>
2015-03-03 23:01:07 -0500

    added FriendFinderGame

---
+++ MiniMap/app/src/main/java/map/minimap/frameworks/FriendFinderGame.java
@@ -0,0 +1,24 @@
+package map.minimap.frameworks;
+
+public class FriendFinderGame extends Game {
+
+	@Override
+	public void processLogic() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void handleMessage() {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+	
+	
+	
+	
+}
