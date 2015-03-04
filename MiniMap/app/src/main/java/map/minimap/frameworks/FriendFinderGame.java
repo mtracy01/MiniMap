9bc004b7b3eb9bdb49d339a52d4f330ccca96a95
Michael Crabill <michael.crabill@gmail.com>
2015-03-03 23:25:03 -0500

    modified game and FriendFinderGame.java

--- MiniMap/app/src/main/java/map/minimap/frameworks/FriendFinderGame.java
+++ MiniMap/app/src/main/java/map/minimap/frameworks/FriendFinderGame.java
@@ -1,7 +1,17 @@
 package map.minimap.frameworks;
 
+import java.util.logging.Logger;
+import android.util.log;
+import server.Beacon;
+import server.Location;
+import server.Server;
+import server.Team;
+import server.User;
+
 public class FriendFinderGame extends Game {
 
+	//private static final Logger log = Logger.getLogger( Server.class.getName() );
+	
 	@Override
 	public void processLogic() {
 		// TODO Auto-generated method stub
@@ -16,7 +26,116 @@ public class FriendFinderGame extends Game {
 		
 		
 	}
+	
+	/**
+	 * Send the game start message to all users
+	 */
+	protected void sendStartMessage() {
+		String message = "gameStart " + id;
+		synchronized (users) {
+			for (User u : users) {
+				u.sendMessage(message);
+			}
+		}
+	}
+	
+	
 
+	
+	@Override
+	/* called when user presses start button
+	 * assign teams, etc...
+	 *  */
+	public void startSession() {
+		log.v("Starting game session " + this.getId());
+		isRunning = true;
+		
+		//Put all users on the same team for friendfinder
+		for (User user: this.users)
+		{
+			teams.get(0).addUser(user);
+		}
+		
+		
+		// Last thing after setting up the game, send the start message
+		sendStartMessage();
+	}
+
+	@Override
+	/*
+	 * everyone is gone? 
+	 * @see sessions.GameSession#endSession()
+	 */
+	public void endSession() {
+		isRunning = false;
+		
+		for (User user: this.users)
+		{
+			removeUser(user);
+		}
+		for (Team team: this.teams)
+		{
+			//might be a temporary solution
+			team.removeAllBeacons();
+		}
+		
+	}
+
+	@Override
+	public void removeUser(User user) {
+		Log.v("Removing user from friendfinder session");
+		user.setInGame(false);
+		user.setGameSession(null);
+		if (getTeambyID(teams, user.getTeamID()) != null) {
+			getTeambyID(teams, user.getTeamID()).removeUser(user);
+		}
+		synchronized (users) {
+			Log.v(users.toString());
+			users.remove(user);
+			Log.v(users.toString());
+			Log.v(users.size() + " users in session");
+			if (users.isEmpty()) {
+				endSession();
+			}
+			if (owner.equals(user) && !users.isEmpty()) {
+				owner = users.get(0);
+			}
+		}
+	}
+
+	@Override
+	/* mid-game */
+	public void addUser(User user, int teamid) {
+		// TODO Auto-generated method stub
+		user.setTeamID(teamid);
+		getTeambyID(teams, teamid).addUser(user);
+		synchronized (users) {
+			users.add(user);
+		}
+	}
+
+	/**
+	 * teamID should always be 0 in FriendFinder
+	 */
+	
+	@Override
+	public void addBeacon(int teamid, Location loc) { 
+		// TODO Auto-generated method stub
+		Beacon beacon = new Beacon(loc);
+		beacon.setTeamId(teamid);
+		getTeambyID(teams, teamid).addBeacon(beacon);
+
+	}
+
+	/**
+	 * teamID should always be 0
+	 */
+	@Override
+	public void removeBeacon(int teamid, Integer id) {
+		// TODO Auto-generated method stub
+		getTeambyID(teams, teamid).removeBeacon((getTeambyID(teams, teamid).getBeaconbyID(id)));
+	}
+	
 	
 	
 	
--- MiniMap/app/src/main/java/map/minimap/frameworks/Game.java
+++ MiniMap/app/src/main/java/map/minimap/frameworks/Game.java
@@ -1,5 +1,13 @@
 package map.minimap.frameworks;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import server.Location;
+import server.Server;
+import server.Team;
+import server.User;
+
 /**
  * Created by joe on 2/21/2015.
  */
@@ -11,8 +19,104 @@ public abstract class Game {
      *      refering to any game type as a Game
      */
 
+	
+	private static Integer baseId = 0;
+	
+	/**
+	 * The game type
+	 */
+	private String gameType;
+	
+	/**
+	 * The game session id
+	 */
+	protected int id;
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	/**
+	 * The server
+	 */
+//	protected Server server;
+	
+	/**
+	 * The owner of the game session (the person who started it)
+	 */
+	protected User owner;
+	
+	/**
+	 * Is the session running?
+	 */
+	protected boolean isRunning;
+
+	/**
+	 * The users in the game session
+	 */
+	protected ArrayList<User> users;
+	
+	/**
+	 * The teams in the game session
+	 */
+	protected ArrayList<Team> teams;
+	
+	
     public Game() {}
     public abstract void processLogic ();
     public abstract void handleMessage();
+	/**
+	 * Start the session
+	 */
+	public abstract void startSession();
+	
+	/**
+	 * End the current session.
+	 * Perform any necessary cleanup.
+	 */
+	public abstract void endSession();
+	
+	/**
+	 * Remove a user from the game session
+	 * @param user The user to remove
+	 */
+	public abstract void removeUser(User user);
+
+	/**
+	 * Add a user to the game session
+	 * @param user
+	 */
+	public abstract void addUser(User user, int teamid);
+	
+	/**
+	 * Add a beacon at the specified location
+	 * @param loc
+	 */
+	public abstract void addBeacon(int teamid, Location loc);
+	
+	/**
+	 * Remove the specified beacon
+	 * @param id
+	 */
+	public abstract void removeBeacon(int teamid, Integer id);
+    
+	public Team getTeambyID(ArrayList<Team> teamlist, int id) {
+		for (Team t: teamlist) {
+			if (t.getTeamID() == id) {
+				return t;
+			}
+		}
+		return null;
+	}
+	
+	
+    
+    
+    
+    
+    
+    
+    
 
 }
