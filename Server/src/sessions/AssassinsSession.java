933fe6230e45d705620bb6e4c887ba22a1b39f9e
Nicki Ogg <oggnicki@yahoo.com>
2015-04-02 13:45:04 -0400

    Added startSession code to assign targets

--- Server/src/sessions/AssassinsSession.java
+++ Server/src/sessions/AssassinsSession.java
@@ -1,14 +1,23 @@
 package sessions;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
+
 import server.Location;
 import server.Server;
 import server.User;
 
 public class AssassinsSession extends GameSession {
+	
+	private static final Logger log = Logger.getLogger( Server.class.getName() );
+	
+	private HashMap<User, User> targets;
 
 	public AssassinsSession(User owner, Server server) {
 		super("assassins", owner, server);
-		// TODO Auto-generated constructor stub
+		
+		targets = new HashMap<User, User>();
 	}
 
 	@Override
@@ -25,8 +34,28 @@ public class AssassinsSession extends GameSession {
 
 	@Override
 	public void startSession() {
-		// TODO Auto-generated method stub
-
+		log.fine("Starting game session " + this.getId());
+		isRunning = true;
+		
+		synchronized (users) {
+			// Assign each user a target
+			User[] usersArray = (User[]) users.toArray();
+			for (int i = 0; i < usersArray.length - 1; i++) {
+				targets.put(usersArray[i], usersArray[i+1]);
+			}
+			targets.put(usersArray[usersArray.length - 1], usersArray[0]);
+		}
+		
+		// Send the start message
+		sendStartMessage();
+		
+		// Send target assignments
+		synchronized (users) {
+			for (Entry<User, User> entry : targets.entrySet()) {
+				String message = "target " + entry.getValue().getUserID();
+				entry.getKey().sendMessage(message);
+			}
+		}
 	}
 
 	@Override
