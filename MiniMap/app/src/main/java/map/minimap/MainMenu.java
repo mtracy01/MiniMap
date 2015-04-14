06f06169e03c52dca368a2a9ed4477e54f80d6af
mtracy01 <matthetracy@gmail.com>
2015-04-14 13:20:44 -0400

    Bug fixes, code cleaning

--- MiniMap/app/src/main/java/map/minimap/MainMenu.java
+++ MiniMap/app/src/main/java/map/minimap/MainMenu.java
@@ -127,83 +127,6 @@ public class MainMenu extends ActionBarActivity
         getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
     }
-
-  /*  @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    /*@Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        switch (position) {
-            case 0:
-                logger.logEvent("Open Games menu");
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, GamesFragment.newInstance("a", "b"))
-                        .commit();
-                break;
-            case 1:
-                logger.logEvent("Open Groups menu");
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, GroupsFragment.newInstance("a", "b"))
-                        .commit();
-                break;
-            case 2:
-                logger.logEvent("Open Friends menu");
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, FriendStatus.newInstance("a", "b"))
-                        .commit();
-                break;
-            case 3:
-                logger.logEvent("Open Invitations menu");
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, InvitationsFragment.newInstance("a", "b"))
-                        .commit();
-                break;
-            case 4:
-                logger.logEvent("Open Settings menu");
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, SettingsFragment.newInstance("a", "b"))
-                        .commit();
-                break;
-        }
-        onSectionAttached(position + 1);
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-            case 4:
-                mTitle = getString(R.string.title_section4);
-                break;
-            case 5:
-                mTitle = getString(R.string.title_section5);
-                break;
-        }
-
-    }*/
-
     public void onFragmentInteraction(Uri uri){
 
     }
@@ -239,56 +162,28 @@ public class MainMenu extends ActionBarActivity
         switch(name){
             case ContentFragment.GAMES:
                 Log.v(LOG_TAG, "Attempting switch to games");
-                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,GamesFragment.newInstance("a","b")).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,GamesFragment.newInstance("a","b")).commit();
                 //We do nothing, so close
                 break;
             case ContentFragment.GROUPS:
-                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,GroupsFragment.newInstance("a","b")).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,GroupsFragment.newInstance("a","b")).commit();
                 break;
             case ContentFragment.FRIENDS:
-                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,FriendStatus.newInstance("a","b")).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,FriendStatus.newInstance("a","b")).commit();
                 break;
             case ContentFragment.SETTINGS:
-                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,SettingsFragment.newInstance("a","b")).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,SettingsFragment.newInstance("a","b")).commit();
                 break;
             case "Logout":
-                //Settings Fragment
-                break;
-            //case 5:
                 //Logout?
-            //    break;
+                break;
         }
 
-
-
-        //getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, contentFragment).commit();
         return contentFragment;
     }
 
     @Override
     public ScreenShotable onSwitch(Resourceble slideMenuItem, ScreenShotable screenShotable, int position) {
-        /*switch(position){
-            case 0:
-                Log.v(LOG_TAG, "" + position);
-                //We do nothing, so close
-                return screenShotable;
-            case 1:
-                //set as Games Fragment
-                break;
-            case 2:
-                //Groups Fragment
-                break;
-            case 3:
-                //Friends Fragment
-                break;
-            case 4:
-                //Settings Fragment
-                break;
-            case 5:
-                //Logout?
-                break;
-        }*/
-
         Log.v(LOG_TAG,""+position);
         switch (slideMenuItem.getName()) {
             case ContentFragment.CLOSE:
