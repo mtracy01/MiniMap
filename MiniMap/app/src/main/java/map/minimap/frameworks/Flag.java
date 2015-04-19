b7baddc489fbda4654c85e4d8117c096c301fdea
Joe <joe.coy@comcast.net>
2015-04-19 15:02:04 -0400

    attempt to use Matthew's cool flag pictures

--- MiniMap/app/src/main/java/map/minimap/frameworks/Flag.java
+++ MiniMap/app/src/main/java/map/minimap/frameworks/Flag.java
@@ -1,7 +1,11 @@
 package map.minimap.frameworks;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -21,9 +25,30 @@ public class Flag {
     private Team team;
     private Marker mapMarker;
 
+    //R.id.drawable.ic_flag_red/blue
+
     public Flag(LatLng location, Team team) {
         this.location = location;
         this.team = team;
+
+        Bitmap big;
+        if (this.team.getTeamID() == 2)
+            big = BitmapFactory.decodeResource(Data.mainAct.getResources(), R.drawable.ic_flag_blue);
+        else if (this.team.getTeamID() == 3) {//red
+            big = BitmapFactory.decodeResource(Data.mainAct.getResources(), R.drawable.ic_flag_red);
+        }
+        else {
+            Log.e("Flag Picture", "Invalid team ID");
+            big = BitmapFactory.decodeResource(Data.mainAct.getResources(), R.drawable.ic_flag_red);
+        }
+
+        big = Bitmap.createScaledBitmap(big,big.getWidth() / 5,big.getHeight() / 5, false);
+        //user.setProfilePhoto(big);
+
+        Bitmap tmp = big;
+        Bitmap doubleSized = Bitmap.createScaledBitmap(tmp,tmp.getWidth() * 2,tmp.getHeight() * 2, false);
+
+        this.mapMarker = BitmapDescriptorFactory.fromBitmap(addBorder(doubleSized, Color.DKGRAY)))));//.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))));
     }
 
     public void show() {
@@ -62,5 +87,7 @@ public class Flag {
     public void setTeam(Team team) {
         this.team = team;
     }
+
+
 
 }
