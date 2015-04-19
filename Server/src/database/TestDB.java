184b013880268c53d6d39222671e3a258d8b4995
Nicki Ogg <oggnicki@yahoo.com>
2015-04-19 14:31:51 -0400

    Server side beacons for ctf

--- Server/src/database/TestDB.java
+++ Server/src/database/TestDB.java
@@ -1,4 +1,4 @@
-import database.DbInteract;
+package database;
 
 public class TestDB {
 	public static void main(String[] args) {
--- Server/src/sessions/CTFSession.java
+++ Server/src/sessions/CTFSession.java
@@ -262,11 +262,15 @@ public class CTFSession extends GameSession {
 	
 	@Override
 	public void addBeacon(int teamid, Location loc) { 
-		// TODO Auto-generated method stub
 		Beacon beacon = new Beacon(loc);
 		beacon.setTeamId(teamid);
 		getTeambyID(teams, teamid).addBeacon(beacon);
-
+		synchronized (users) {
+			String message = "addbeacon " + beacon.getId() + " " + loc.getLatitude() + " " + loc.getLongitude();
+			for (User u : users) {
+				u.sendMessage(message);
+			}
+		}
 	}
 
 	/**
@@ -274,8 +278,13 @@ public class CTFSession extends GameSession {
 	 */
 	@Override
 	public void removeBeacon(int teamid, Integer id) {
-		// TODO Auto-generated method stub
 		getTeambyID(teams, teamid).removeBeacon((getTeambyID(teams, teamid).getBeaconbyID(id)));
+		synchronized (users) {
+			String message = "removebeacon " + id;
+			for (User u : users) {
+				u.sendMessage(message);
+			}
+		}
 	}
 
 	@Override
