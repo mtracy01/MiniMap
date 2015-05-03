fd25fc54e23ab096ca8f590030a16339866a8793
mtracy01 <matthetracy@gmail.com>
2015-05-03 19:56:41 -0400

    minor code cleaning

--- MiniMap/app/src/main/java/map/minimap/games/captureTheFlag/CTFflags.java
+++ MiniMap/app/src/main/java/map/minimap/games/captureTheFlag/CTFflags.java
@@ -165,9 +165,6 @@ public class CTFflags extends FragmentActivity implements OnMapReadyCallback{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
